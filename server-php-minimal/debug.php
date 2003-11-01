a57a81fa63530e55e74bb3bde422d038d78c5400
siebert <siebert>
2003-11-01 12:44:58 +0000

    NEW: Copyright

--- server-php-minimal/debug.php
+++ server-php-minimal/debug.php
@@ -1,4 +1,30 @@
 <?php
+// ----------------------------------------------------------------------------
+// debug.php
+// Copyright (C) 2003 Steffen Siebert mybookmarksync@steffensiebert.de
+//
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software Foundation,
+// Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+// ----------------------------------------------------------------------------
+// This library is GPL'd.  If you distribute this program or a derivative of
+// this program publicly you must include the source code.  It is easy
+// enough to drop us an email requesting a different license, if necessary.
+//
+// Description: Debug log interface
+// Created:     October 2003, Steffen Siebert
+// Modified:    $Date: 2003/11/01 12:44:58 $, $Author: siebert $
+// ----------------------------------------------------------------------------
 session_start();
 $passwd = trim($_POST["passwd"]);
 if ($passwd == 'secret') {
